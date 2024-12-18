9b8bb62f996b1ef70d5439a4c9eef0b0e8b598b3
ThanieaLuqizahwa <114566542+ThanieaLuqizahwa@users.noreply.github.com>
2024-12-19 01:18:45 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,9 @@ app.use(
 
 const db = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "",
-  database: "ayune_database",
+  user: "tom",
+  password: "Tom-1234",
+  database: "dcac",
 });
 
 db.connect((err) => {
