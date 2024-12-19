0fa4da683675bf791651b1b034072543b929fd17
ThanieaLuqizahwa <114566542+ThanieaLuqizahwa@users.noreply.github.com>
2024-12-19 15:46:43 +0700

    Update SignUp.jsx

--- my-app/src/SignUp.jsx
+++ my-app/src/SignUp.jsx
@@ -15,7 +15,7 @@ const SignUp = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:5000/api/signup", {
+    const response = await fetch("http://localhost:3011/api/signup", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
